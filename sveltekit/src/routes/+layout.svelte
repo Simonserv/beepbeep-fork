8fc9513fd016c213b5a5dbba448d14a69e4fa199
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-07-20 03:10:31 +0800

    chore(sveltekit): separate navbar component

--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -1,24 +1,7 @@
 <script>
     import "../app.css";
-    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+    import BeepbeepNavbar from "./BeepbeepNavbar.svelte";
 </script>
 
-<Navbar>
-    <NavBrand href="/">
-        <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
-        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Beepbeep</span>
-    </NavBrand>
-
-    <NavHamburger/>
-
-    <NavUl>
-        <NavLi href="/">Home</NavLi>
-        <NavLi href="/map">Map</NavLi>
-        <NavLi href="/about">About</NavLi>
-        <NavLi href="/docs/components/navbar">Navbar</NavLi>
-        <NavLi href="/pricing">Pricing</NavLi>
-        <NavLi href="/contact">Contact</NavLi>
-    </NavUl>
-</Navbar>
-
+<BeepbeepNavbar/>
 <slot/>
---
+++ sveltekit/src/routes/BeepbeepNavbar.svelte
@@ -0,0 +1,18 @@
+<script>
+    import "../app.css";
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<Navbar>
+    <NavBrand href="/">
+        <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Beepbeep</span>
+    </NavBrand>
+
+    <NavHamburger/>
+
+    <NavUl>
+        <NavLi href="/">Home</NavLi>
+        <NavLi href="/map">Map</NavLi>
+    </NavUl>
+</Navbar>
