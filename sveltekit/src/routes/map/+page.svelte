563135980b9d04c63b17d3270190b73543eefb72
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-08-08 04:37:38 +0800

    feat(`/map`): add top left drawer

--- sveltekit/src/routes/map/+page.svelte
+++ sveltekit/src/routes/map/+page.svelte
@@ -1,5 +1,16 @@
-<script>
+<script lang="ts">
+    import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
+    import { DrawSquareOutline } from "flowbite-svelte-icons";
     import OpenLayersMap from "./OpenLayersMap.svelte";
 </script>
 
-<OpenLayersMap/>
+<div class="overflow-hidden">
+    <OpenLayersMap/>
+    <Button size="xs" color="light" class="absolute left-4 top-16"><DrawSquareOutline/></Button>
+    <Dropdown>
+        <DropdownItem>Dashboard</DropdownItem>
+        <DropdownItem>Settings</DropdownItem>
+        <DropdownItem>Earnings</DropdownItem>
+        <DropdownItem slot="footer">Sign out</DropdownItem>
+    </Dropdown>
+</div>
