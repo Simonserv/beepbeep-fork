ea186b810466e960b878f34bbe8a72a3be5f7977
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-08-08 05:00:27 +0800

    feat(`/map`): add top right button group

--- sveltekit/src/routes/map/+page.svelte
+++ sveltekit/src/routes/map/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
-    import { DrawSquareOutline } from "flowbite-svelte-icons";
+    import { Avatar, Button, ButtonGroup, Dropdown, DropdownItem } from "flowbite-svelte";
+    import { DrawSquareOutline, PlusOutline, WalletOutline } from "flowbite-svelte-icons";
     import OpenLayersMap from "./OpenLayersMap.svelte";
 </script>
 
@@ -13,4 +13,15 @@
         <DropdownItem>Earnings</DropdownItem>
         <DropdownItem slot="footer">Sign out</DropdownItem>
     </Dropdown>
+
+    <ButtonGroup class="*:!ring-primary-700 absolute right-4 top-16">
+        <Button size="xs">
+            <div class="flex space-x-4 rtl:space-x-reverse">
+                <Avatar size="xs" src="https://s3-alpha.figma.com/hub/file/4338730091/25fd46ec-d7b2-4052-830e-674d22fe7f8c-cover.png" />
+            </div>
+        </Button>
+        <Button size="xs" color="light" disabled >1020</Button>
+        <Button size="xs" color="light"><PlusOutline/></Button>
+        <Button size="xs" color="primary"><WalletOutline/></Button>
+    </ButtonGroup>
 </div>
