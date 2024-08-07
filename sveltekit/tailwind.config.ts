e1a9c0104a47394fd9fa52d460c27e0ba4db6f85
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-08-07 18:04:06 +0800

    style: set page to be dark theme by default

--- sveltekit/tailwind.config.ts
+++ sveltekit/tailwind.config.ts
@@ -2,6 +2,8 @@ import flowbitePlugin from 'flowbite/plugin'
 import type { Config } from 'tailwindcss';
 
 export default {
+    darkMode: 'class',
+
 	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
 
 	theme: {
