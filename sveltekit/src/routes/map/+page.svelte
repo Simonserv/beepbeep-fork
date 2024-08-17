3a354bf8c4c35d65c987588ec29a246db9374ddb
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-08-17 14:32:16 +0800

    fix(`/map`): overlay button groups on map

--- sveltekit/src/routes/map/+page.svelte
+++ sveltekit/src/routes/map/+page.svelte
@@ -4,27 +4,29 @@
     import OpenLayersMap from "./OpenLayersMap.svelte";
 </script>
 
-<div class="overflow-hidden">
+<div class="overflow-hidden h-screen relative flex z-0">
     <OpenLayersMap/>
 
-    <!-- Top left drawer-->
-    <Button size="xs" color="light" class="absolute left-4 top-16"><DrawSquareOutline/></Button>
-    <Dropdown>
-        <DropdownItem>Dashboard</DropdownItem>
-        <DropdownItem>Settings</DropdownItem>
-        <DropdownItem>Earnings</DropdownItem>
-        <DropdownItem slot="footer">Sign out</DropdownItem>
-    </Dropdown>
+    <div class="grid grid-cols-2">
+        <!-- Top left drawer-->
+        <Button size="xs" color="light" class="absolute left-4 top-4"><DrawSquareOutline/></Button>
+        <Dropdown>
+            <DropdownItem>Dashboard</DropdownItem>
+            <DropdownItem>Settings</DropdownItem>
+            <DropdownItem>Earnings</DropdownItem>
+            <DropdownItem slot="footer">Sign out</DropdownItem>
+        </Dropdown>
 
-    <!-- Top right button group -->
-    <ButtonGroup class="*:!ring-primary-700 absolute right-4 top-16">
-        <Button size="xs">
-            <div class="flex space-x-4 rtl:space-x-reverse">
-                <Avatar size="xs" src="https://s3-alpha.figma.com/hub/file/4338730091/25fd46ec-d7b2-4052-830e-674d22fe7f8c-cover.png" />
-            </div>
-        </Button>
-        <Button size="xs" color="light" disabled >1020</Button>
-        <Button size="xs" color="light"><PlusOutline/></Button>
-        <Button size="xs" color="primary"><WalletOutline/></Button>
-    </ButtonGroup>
+        <!-- Top right button group -->
+        <ButtonGroup class="*:!ring-primary-700 absolute right-4 top-4">
+            <Button size="xs">
+                <div class="flex space-x-4 rtl:space-x-reverse">
+                    <Avatar size="xs" src="https://s3-alpha.figma.com/hub/file/4338730091/25fd46ec-d7b2-4052-830e-674d22fe7f8c-cover.png" />
+                </div>
+            </Button>
+            <Button size="xs" color="light" disabled >1020</Button>
+            <Button size="xs" color="light"><PlusOutline/></Button>
+            <Button size="xs" color="primary"><WalletOutline/></Button>
+        </ButtonGroup>
+    </div>
 </div>
