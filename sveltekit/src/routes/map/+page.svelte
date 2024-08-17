a9f12d05968a967b750310dd40ab0d16459a9566
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-08-17 14:37:50 +0800

    feat(`/map`): overlay location card on map

--- sveltekit/src/routes/map/+page.svelte
+++ sveltekit/src/routes/map/+page.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
-    import { Avatar, Button, ButtonGroup, Dropdown, DropdownItem } from "flowbite-svelte";
-    import { DrawSquareOutline, PlusOutline, WalletOutline } from "flowbite-svelte-icons";
+    import { Avatar, Button, ButtonGroup, Card, Dropdown, DropdownItem } from "flowbite-svelte";
+    import { MapPinAltSolid, } from "flowbite-svelte-icons";
+    import { ArrowUpDownOutline, DrawSquareOutline, DotsVerticalOutline, PlusOutline, WalletOutline } from "flowbite-svelte-icons";
     import OpenLayersMap from "./OpenLayersMap.svelte";
+
+    const BUTTON_GRID = {
+        "Restaurant": "resto-icon",
+        "Gym": "gym-icon",
+        "Hospital": "hospital-icon",
+        "Mall": "mall-icon",
+        "Pet Shop": "pet-icon",
+        "Beach": "beach-icon",
+        "Library": "library-icon",
+        "See more": "seemore-icon",
+    }
+
+    let source_location_first = true;
+
+    function switch_location_inputs() {
+        source_location_first = !source_location_first;
+        console.log(source_location_first);
+    }
 </script>
 
 <div class="overflow-hidden h-screen relative flex z-0">
@@ -29,4 +48,78 @@
             <Button size="xs" color="primary"><WalletOutline/></Button>
         </ButtonGroup>
     </div>
+
+
+    <!--
+        Buttom navigation card with:
+        + start/end point
+        + current location buttons
+        + suggested travel destination
+    -->
+    <Card class="absolute m-auto bottom-4 left-0 right-0 mt-4">
+        <div class="grid grid-cols-[1fr_3fr_1fr] mb-8">
+            <!-- Left side -->
+            <div class="grid grid-rows-3 justify-center">
+                <Button pill={true} class="!w-fit !h-fit !p-2" ><MapPinAltSolid class="h-4 w-4" /></Button>
+                <div class=""> <DotsVerticalOutline /> </div>
+                <Button pill={true} class="!w-fit !h-fit !p-2"><MapPinAltSolid class="h-4 w-4" /></Button>
+            </div>
+
+            <!-- Middle side -->
+            <div class="grid grid-rows-3">
+                {#if source_location_first}
+                <Button pill={true} color="alternative" size="xs">
+                    <div class="text-xs">
+                        <span>Where to?</span>
+                        <span>Choose destination</span>
+                    </div>
+                </Button>
+                <div></div>
+                <Button pill={true} color="alternative" size="xs">
+                    <div class="text-xs">
+                        <span>Choose starting location</span>
+                    </div>
+                </Button>
+                {:else}
+                <Button pill={true} color="alternative" size="xs">
+                    <div class="text-xs">
+                        <span>Choose starting location</span>
+                    </div>
+                </Button>
+                <div></div>
+                <Button pill={true} color="alternative" size="xs">
+                    <div class="text-xs">
+                        <span>Where to?</span>
+                        <span>Choose destination</span>
+                    </div>
+                </Button>
+                {/if}
+            </div>
+
+            <!-- Right side -->
+            <div class="grid grid-rows-3">
+                <div></div>
+                <button on:click={switch_location_inputs}> <ArrowUpDownOutline on:click={switch_location_inputs} /> </button>
+                <div></div>
+            </div>
+        </div>
+
+        <div class="inline-flex items-center justify-center w-full mb-4">
+            <hr class="w-full h-px bg-gray-200 border-0 dark:bg-gray-700"/>
+        </div>
+
+        <div>
+            Suggested Travel Destinations
+        </div>
+
+        <div class="grid grid-rows-2 grid-cols-4">
+            {#each Object.entries(BUTTON_GRID) as [name, icon]}
+            <div class="grid grid-rows-2">
+                <Button color="alternative" size="xs">{icon}</Button>
+                {name}
+            </div>
+            {/each}
+        </div>
+    </Card>
+
 </div>
